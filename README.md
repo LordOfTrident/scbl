8595e8c1a6e7992c00901eb9c27c4b5f3ff8167b
Lenovo <lordoftrident@gmail.com>
2021-07-21 13:57:43 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,83 @@
+# SCBL
+Simple Command Bar Language
+
+<img src="pic/logo.png"/>
+
+A simple, small scripting language to be embedded with a C++ program.
+It is constructed of functions and parameters:
+```
+<function> <parameters>;
+```
+the parameters can be constants, integers or strings.
+#### Strings
+Strings start and end with `"`. The parser turns the characters into separate integer parameters by their ascii codes and automatically puts a NULL Terminator after each string as an integer parameter, but if you put `~` after the string, it wont do so. Example using the SCBL Shell:
+```
+printr "A";
+printr "A"~;
+```
+First `printr` would output `650` (As a combination of `A` (65) and the NULL Terminator (0)) and the second one would output `65`.
+
+## Compiling and running the shell
+#### Linux
+Run `compile` as bash in the terminal:
+```sh
+bash compile
+```
+then run scbl:
+```sh
+scbl
+```
+#### Windows
+First make sure you have g++ installed (Command Prompt will not recognize the `g++` command if you dont) and run the following commands:
+```bat
+mkdir bin
+g++ -g shell/main.cc -o bin/scbl -O3 -s -Wall -std=c++17;
+```
+then run scbl:
+```bat
+scbl
+```
+
+## Embedding with C++
+SCBL is really small and simple, so you only need to include the header file `scbl.hh` in your C++ program and then create an object of SCBL_Interpreter class which, as everything else SCBL-related, can be found inside the SCBL namespace. List of SCBL_Interpreter methods:
+- `void AddFunction(function)`: Pushes the function to the functions stack
+- `void SetFunction(function)`: Rewrites, or if not found, pushes the function to the functions stack
+- `void AddConstant(constant)`: Pushes the constant to the constants stack
+- `void SetConstant(constant)`: Rewrites, or if not found, pushes the constant to the constants stack
+- `uint8_t Parse(text)`: Parses the text into tokens, returns `SCBL_PARSER_OK` (0) if no errors occured, else returns `SCBL_PARSER_ERROR` (1)
+- `uint8_t Run()`: Interprets the parsed text, returns `SCBL_RUNTIME_OK` (0) if no errors occured, else returns `SCBL_RUNTIME_ERROR` (1)
+- `std::string GetErrorMsg()`: Returns the description of the last error
+
+`Function` and `Constant` are two other classes inside the SCBL namespace. `Function` holds the Name (std::string) and Callback (void (*callback)(std::vector <uint32_t>)) and `Constant` holds the Name (std::string) and Value (uint32_t). `Function` and `Constant` constructors:
+- Function(name, callback)
+- Constant(name, value)
+
+#### Example:
+```cc
+#include <iostream>
+#include "scbl.hh"
+
+int main() {
+    SCBL::SCBL_Interpreter SCBLi;
+    
+    SCBLi.AddConstant(SCBL::Constant("newline", 10));
+    SCBLi.AddFunction(SCBL::Function("print", [] (std::vector <uint32_t> Parameters) {
+        for (uint32_t i = 0; i < (uint32_t)Parameters.size(); ++ i) 
+            std::cout << (char)Parameters[i];
+    }));
+    
+    if (SCBLi.Parse("print \"Hello, World!\"~ newline;") == SCBL_PARSER_ERROR) {
+        std::cout << SCBLi.GetErrorMsg() << std::endl;
+        
+        return 0;
+    };
+    
+    if (SCBLi.Run() == SCBL_RUNTIME_ERROR) {
+        std::cout << SCBLi.GetErrorMsg() << std::endl;
+        
+        return 0;
+    };
+    
+    return 0;
+};
+```
---
+++ scbl.hh
@@ -0,0 +1,252 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <cstdint>
+#include <algorithm>
+
+#define SCBL_TYPE_INTEGER 0
+#define SCBL_TYPE_FUNCTION 1
+#define SCBL_CMD_END 2
+
+#define SCBL_RUNTIME_OK 0
+#define SCBL_PARSER_OK 0
+#define SCBL_RUNTIME_ERROR 1
+#define SCBL_PARSER_ERROR 1
+
+typedef int8_t  i8;
+typedef int16_t i16;
+typedef int32_t i32;
+
+typedef uint8_t  ui8;
+typedef uint16_t ui16;
+typedef uint32_t ui32;
+
+namespace SCBL {
+    typedef void (*callback)(std::vector <ui32>);
+    
+    class Token {
+    public: 
+    	Token(ui8 p_Type, ui32 p_Data):
+    	    Type(p_Type),
+    	    Data(p_Data) 
+    	{};
+
+        ui8 Type;
+        ui32 Data;
+    };
+
+    class Function {
+    public: 
+    	Function(std::string p_Name, callback p_Callback):
+    	    Name(p_Name),
+    	    Callback(p_Callback) 
+    	{};
+
+        std::string Name;
+        callback Callback;
+    };
+
+    class Constant {
+    public: 
+    	Constant(std::string p_Name, ui32 p_Value):
+    	    Name(p_Name),
+    	    Value(p_Value) 
+    	{};
+
+        std::string Name;
+        ui32 Value;
+    };
+
+    class SCBL_Interpreter {
+    public: 
+    	SCBL_Interpreter() {};
+
+        i8 Parse(std::string p_Code) {
+            ErrorMsg = "";
+            Tokens = {};
+        
+            if (p_Code == "") 
+            	return SCBL_PARSER_OK;
+        
+            std::string token;
+            for (i32 i = 0; i < (i32)p_Code.length(); ++ i) {
+                token = "";
+        
+                while (p_Code[i] <= 32 and p_Code[i] >= 0)
+                	++ i;
+        
+                if ((p_Code[i] <= 32 and p_Code[i] >= 0) or i > (i32)p_Code.length()) 
+                    continue;
+                else if (p_Code[i] >= '0' and p_Code[i] <= '9') {
+                    while (p_Code[i] >= '0' and p_Code[i] <= '9') {
+                        token += p_Code[i];
+        
+                        ++ i;
+                    };
+                    
+                    if (not (p_Code[i] <= 32 and p_Code[i] >= 0) and p_Code[i] != ';' and p_Code[i] != '{') {
+                        ErrorMsg = (std::string) "Unexpected symbol \"" + p_Code[i] + "\" in number";
+        
+                        return SCBL_PARSER_ERROR;
+                    };
+        
+                    Tokens.push_back(Token(SCBL_TYPE_INTEGER, std::stoi(token)));
+        
+                    -- i;
+                } else if (p_Code[i] == '"') {
+                	++ i;
+
+                	while (p_Code[i] != '"' and not (i > (i32)p_Code.length())) {
+                    	Tokens.push_back(Token(SCBL_TYPE_INTEGER, p_Code[i]));
+
+                		++ i;
+                	};
+
+                	if (p_Code[i] != '"') {
+                        ErrorMsg = "String not closed";
+
+                		return SCBL_PARSER_ERROR;
+                	};
+
+                	++ i;
+
+                	if (p_Code[i] != '~')
+	                    Tokens.push_back(Token(SCBL_TYPE_INTEGER, 0));
+                	else 
+                		++ i;
+                	
+                	if (not (p_Code[i] <= 32 and p_Code[i] >= 0) and p_Code[i] != ';' and p_Code[i] != '{') {
+                		ErrorMsg = "Unexpected symbol after string end";
+
+                		return SCBL_PARSER_ERROR;
+                	};
+
+                	-- i;
+                } else if (p_Code[i] == '{') {
+                	++ i;
+                	
+                	while (p_Code[i] != '}' and not (i > (i32)p_Code.length()))
+                		++ i;
+
+                	if (p_Code[i] != '}') {
+                		ErrorMsg = "Comment not closed";
+
+                		return SCBL_PARSER_ERROR;
+                	};
+                } else if (p_Code[i] == '#') {
+	               	++ i;
+	               	
+	               	while (p_Code[i] != 10 and not (i > (i32)p_Code.length()))
+	               		++ i;
+                } else if (p_Code[i] == ';') 
+                    Tokens.push_back(Token(SCBL_CMD_END, 0));
+                else {
+                    while (not (p_Code[i] <= 32 and p_Code[i] >= 0) and p_Code[i] != ';' and not (i > (i32)p_Code.length())) {
+                        token += p_Code[i];
+                        
+                        ++ i;
+                    };
+        
+                    auto FuncIdx = std::find_if(Functions.begin(), Functions.end(), [token] (const SCBL::Function& p_func) {
+                        return p_func.Name == token;
+                    });
+        
+                    if (FuncIdx != Functions.end()) 
+                        Tokens.push_back(Token(SCBL_TYPE_FUNCTION, std::distance(Functions.begin(), FuncIdx)));
+                    else {
+                        auto ConstIdx = std::find_if(Constants.begin(), Constants.end(), [token] (const SCBL::Constant& p_const) {
+                            return p_const.Name == token;
+                        });
+        
+                        if (ConstIdx != Constants.end()) 
+                            Tokens.push_back(Token(SCBL_TYPE_INTEGER, Constants[std::distance(Constants.begin(), ConstIdx)].Value));
+                        else {
+                            ErrorMsg = "Identifier \"" + token + "\" not found";
+        
+                            return SCBL_PARSER_ERROR;
+                        };
+                    };
+        
+                    -- i;
+                };
+            };
+        
+            return SCBL_PARSER_OK;
+        };
+        
+        i8 Run()  {
+		    for (i32 i = 0; i < (i32)Tokens.size(); ++ i) {
+		        if (Tokens[i].Type != SCBL_TYPE_FUNCTION) {
+		            ErrorMsg = "[" + std::to_string((int)i) + "] Expected function, got (" + (Tokens[i].Type == SCBL_CMD_END? "CMD_END, " : "INT, ") + ".Data:" + std::to_string(Tokens[i].Data) + ")";
+
+		            return SCBL_RUNTIME_ERROR;
+		        };
+
+		        ui32 FuncIdx = Tokens[i].Data;
+		        std::vector <ui32> Params;
+
+		        ++ i;
+
+		        while (i < (i32)Tokens.size() and Tokens[i].Type != SCBL_CMD_END) {
+		            if (Tokens[i].Type != SCBL_TYPE_INTEGER) {
+		                ErrorMsg = "[" + std::to_string(i) + "] Expected integer, got (" + (Tokens[i].Type == SCBL_CMD_END? "CMD_END, " : "FUNC, ") + ".Data:" + std::to_string(Tokens[i].Data) + ")";
+
+		                return SCBL_RUNTIME_ERROR;
+		            };
+
+		            Params.push_back(Tokens[i].Data);
+
+		            ++ i;
+		        };
+
+		        Functions[FuncIdx].Callback(Params);
+		    };
+
+		    return SCBL_RUNTIME_OK;
+		};
+		
+        void AddFunction(Function p_func) {
+            Functions.push_back(p_func);
+        };
+
+        void SetFunction(Function p_func) {
+        	for (ui32 i = 0; i < (ui32)Functions.size(); ++ i) {
+        		if (Functions[i].Name == p_func.Name) {
+        			Functions[i] = p_func;
+
+        			return;
+        		};
+        	};
+        	
+            Functions.push_back(p_func);
+        };
+        
+        void AddConstant(Constant p_const) {
+            Constants.push_back(p_const);
+        };
+
+        void SetConstant(Constant p_const) {
+        	for (ui32 i = 0; i < (ui32)Constants.size(); ++ i) {
+        		if (Constants[i].Name == p_const.Name) {
+        			Constants[i] = p_const;
+
+        			return;
+        		};
+        	};
+        	
+            Constants.push_back(p_const);
+        };
+        
+        std::string GetErrorMsg() {
+            return ErrorMsg;
+        };
+        
+    private:
+        std::string ErrorMsg;
+
+        std::vector <Token> Tokens;
+        std::vector <Function> Functions;
+        std::vector <Constant> Constants;
+    };
+};
