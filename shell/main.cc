21747cb49ea581b51bce2a27410ffcb2fd75cfad
Lenovo <lordoftrident@gmail.com>
2021-07-27 20:44:16 +0200

    fixed shell file interpreting

--- shell/main.cc
+++ shell/main.cc
@@ -5,31 +5,31 @@
 #include "../scbl.hh"
 
 bool FileExists(std::string p_name) {
-    std::ifstream fhnd(p_name);
+	std::ifstream fhnd(p_name);
     
-    return fhnd.good();
+	return fhnd.good();
 };
 
 std::string ReadFile(std::string p_fname) {
-    std::string fstr = "";
-    std::ifstream fhnd(p_fname);
+	std::string fstr = "";
+	std::ifstream fhnd(p_fname);
 
-    if (fhnd.is_open()) {
-        std::string fln = "";
+	if (fhnd.is_open()) {
+		std::string fln = "";
 
-        while (getline(fhnd, fln))
-            fstr += fln + '\n';
-    };
+		while (getline(fhnd, fln))
+			fstr += fln + '\n';
+	};
 
-    fhnd.close();
+	fhnd.close();
 
-    return fstr;
+	return fstr;
 };
 
 void WriteFile(std::string p_fname, std::string p_txt) {
-    std::ofstream fhnd(p_fname);
+	std::ofstream fhnd(p_fname);
 
-    fhnd << p_txt;
+	fhnd << p_txt;
 };
 
 bool running = false;
@@ -37,7 +37,7 @@ ui8 exitcode = 0;
 
 int main(int argc, char* argv[]) {
 	SCBL::SCBL_Interpreter SCBLi;
-	
+
 	SCBLi.AddConstant(SCBL::Constant("NUL", 0));
 	SCBLi.AddConstant(SCBL::Constant("SOH", 1));
 	SCBLi.AddConstant(SCBL::Constant("STX", 2));
@@ -76,92 +76,92 @@ int main(int argc, char* argv[]) {
 		SCBLi.AddConstant(SCBL::Constant(std::string(1, i), i));
 	
 	SCBLi.AddFunction(SCBL::Function("print", [] (std::vector <ui32> Parameters) {
-	    for (ui32 i = 0; i < (ui32)Parameters.size(); ++ i) 
-	        std::cout << (char)Parameters[i];
+		for (ui32 i = 0; i < (ui32)Parameters.size(); ++ i) 
+			std::cout << (char)Parameters[i];
 	}));
 	
 	SCBLi.AddFunction(SCBL::Function("printf", [] (std::vector <ui32> Parameters) {
-	    for (ui32 i = 0; i < (ui32)Parameters.size(); ++ i) 
-	        std::cout << (char)Parameters[i];
+		for (ui32 i = 0; i < (ui32)Parameters.size(); ++ i) 
+			std::cout << (char)Parameters[i];
 	    
-	    std::cout << std::endl;
+		std::cout << std::endl;
 	}));
 	
 	SCBLi.AddFunction(SCBL::Function("printfr", [] (std::vector <ui32> Parameters) {
-	    for (ui32 i = 0; i < (ui32)Parameters.size(); ++ i) 
-	        std::cout << Parameters[i];
+		for (ui32 i = 0; i < (ui32)Parameters.size(); ++ i) 
+			std::cout << Parameters[i];
 
-	    std::cout << std::endl;
+		std::cout << std::endl;
 	}));
 	
 	SCBLi.AddFunction(SCBL::Function("printr", [] (std::vector <ui32> Parameters) {
-	    for (ui32 i = 0; i < (ui32)Parameters.size(); ++ i) 
-	        std::cout << Parameters[i];
+		for (ui32 i = 0; i < (ui32)Parameters.size(); ++ i) 
+			std::cout << Parameters[i];
 	}));
 
 	SCBLi.AddFunction(SCBL::Function("cprintr", [] (std::vector <ui32> Parameters) {
-	    for (ui32 i = 0; i < (ui32)Parameters.size(); ++ i) 
-	        printf("%i", Parameters[i]);
+		for (ui32 i = 0; i < (ui32)Parameters.size(); ++ i) 
+			printf("%i", Parameters[i]);
 	}));
 	
 	SCBLi.AddFunction(SCBL::Function("cprint", [] (std::vector <ui32> Parameters) {
-	    for (ui32 i = 0; i < (ui32)Parameters.size(); ++ i) 
-	        printf("%c", Parameters[i]);
+		for (ui32 i = 0; i < (ui32)Parameters.size(); ++ i) 
+			printf("%c", Parameters[i]);
 	}));
 
 	SCBLi.AddFunction(SCBL::Function("puts", [] (std::vector <ui32> Parameters) {
 		std::string str = "";
 		
-	    for (ui32 i = 0; i < (ui32)Parameters.size(); ++ i) 
-	        str += std::string(1, Parameters[i]);
+		for (ui32 i = 0; i < (ui32)Parameters.size(); ++ i) 
+			str += std::string(1, Parameters[i]);
 
-	    puts(str.c_str());
+		puts(str.c_str());
 	}));
 	
 	SCBLi.AddFunction(SCBL::Function("putr", [] (std::vector <ui32> Parameters) {
 		std::string str = "";
 	
-	    for (ui32 i = 0; i < (ui32)Parameters.size(); ++ i) 
-	    	str += std::to_string(Parameters[i]);
+		for (ui32 i = 0; i < (ui32)Parameters.size(); ++ i) 
+			str += std::to_string(Parameters[i]);
 
-	    puts(str.c_str());
+		puts(str.c_str());
 	}));
 
 	SCBLi.AddFunction(SCBL::Function("stof", [] (std::vector <ui32> Parameters) {
 		std::string fname = "";
 		std::string fcontents = "";
 
-	    for (ui32 i = 0; i < (ui32)Parameters.size() and Parameters[i] != 0; ++ i)
-	        fname += std::string(1, Parameters[i]);
+		for (ui32 i = 0; i < (ui32)Parameters.size() and Parameters[i] != 0; ++ i)
+			fname += std::string(1, Parameters[i]);
 	    
-	    for (ui32 i = fname.length() + 1; i < (ui32)Parameters.size() and Parameters[i] != 0; ++ i) 
-	        fcontents += std::string(1, Parameters[i]);
+		for (ui32 i = fname.length() + 1; i < (ui32)Parameters.size() and Parameters[i] != 0; ++ i) 
+			fcontents += std::string(1, Parameters[i]);
 	    
-	    WriteFile(fname, fcontents);
+		WriteFile(fname, fcontents);
 	}));
 	
 	SCBLi.AddFunction(SCBL::Function("exit", [] (std::vector <ui32> Parameters) {
-	    running = false;
+		running = false;
 	    
-	    if (Parameters.size() >= 1)
-	    	exitcode = Parameters[0];
+		if (Parameters.size() >= 1)
+			exitcode = Parameters[0];
 	}));
 	
 	SCBLi.AddFunction(SCBL::Function("help", [] (std::vector <ui32> Parameters) {
-	    std::cout << "Available functions:\n";
-	    std::cout << " Console functions:\n";
-	    std::cout << "  print   <...> - Print out encoded characters using std::cout\n";
-	    std::cout << "  printf  <...> - Print out encoded characters using std::cout and flush the stream\n";
-	    std::cout << "  cprint  <...> - Print out encoded characters using printf\n";
-	    std::cout << "  puts    <...> - Print out a string using puts\n";
-	    std::cout << "  printr  <...> - Print out raw integers using std::cout\n";
-	    std::cout << "  printfr <...> - Print out raw integers using std::cout and flush the stream\n";
-	    std::cout << "  cprintr <...> - Print out raw integers using printf\n";
-	    std::cout << "  putr    <...> - Print out raw integers using puts\n";
-	    std::cout << "\n File functions:\n";
-	    std::cout << "  stof <filename> <contents> - Write a file\n";
-	    std::cout << "\n Other functions:\n";
-	    std::cout << "  exit <exitcode> - Exit the program\n";
+		std::cout << "Available functions:\n";
+		std::cout << " Console functions:\n";
+		std::cout << "  print   <...> - Print out encoded characters using std::cout\n";
+		std::cout << "  printf  <...> - Print out encoded characters using std::cout and flush the stream\n";
+		std::cout << "  cprint  <...> - Print out encoded characters using printf\n";
+		std::cout << "  puts    <...> - Print out a string using puts\n";
+		std::cout << "  printr  <...> - Print out raw integers using std::cout\n";
+		std::cout << "  printfr <...> - Print out raw integers using std::cout and flush the stream\n";
+		std::cout << "  cprintr <...> - Print out raw integers using printf\n";
+		std::cout << "  putr    <...> - Print out raw integers using puts\n";
+		std::cout << "\n File functions:\n";
+		std::cout << "  stof <filename> <contents> - Write a file\n";
+		std::cout << "\n Other functions:\n";
+		std::cout << "  exit <exitcode> - Exit the program\n";
 	}));
 
 	if (argc > 1) {
@@ -173,15 +173,17 @@ int main(int argc, char* argv[]) {
 
 		if (SCBLi.Parse(ReadFile(argv[1])) != SCBL_PARSER_OK) {
 			std::cout << SCBLi.GetErrorMsg() << std::endl;
-			
+
 			return 0;
 		};
-		
+
 		if (SCBLi.Run() != SCBL_RUNTIME_OK) {
 			std::cout << SCBLi.GetErrorMsg() << std::endl;
-			
+
 			return 0;
 		};
+
+		return exitcode;
 	};
 
 	std::cout << "SCBL Shell\nFor help type in 'help'." << std::endl;
